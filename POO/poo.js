2d348e524b499f1895bb31b5884c7691a3fbb13e
donCelis <alexcelis019@gmail.com>
2024-04-23 22:20:45 -0500

    herencia

--- POO/poo.js
+++ POO/poo.js
@@ -13,14 +13,16 @@ console.log(fecha);
 // crear una clase
 class Persona {
   //atributos del objeto
-  constructor({ nombre, apellido, calificacionFinal = 0 }) {
+  constructor({ nombre, apellido }) {
     this.nombre = nombre;
     this.apellido = apellido;
-    this.calificacionFinal = calificacionFinal;
+    this.nombreCompleto = `${this.nombre} ${apellido}`;
   }
 
   // métodos del objeto
-  nombreCompleto() {
+  nombreCompletoMethod() {
+    // this.nombre = nuevoNombre;
+    // this.ejemplo = "lo que sea"
     return `${this.nombre} ${this.apellido}`;
   }
 }
@@ -28,10 +30,59 @@ class Persona {
 const cesarClass = new Persona({
   nombre: "Cesar",
   apellido: "Vivas",
-  calificacionFinal: 0,
 });
-
-console.log(cesarClass.nombre);
-console.log(cesarClass.nombreCompleto());
+// console.log(cesarClass.nombre);
+// console.log(cesarClass.nombreCompletoMethod());
 
 // Herencia
+class Estudiante extends Persona {
+  constructor({ nombre, apellido, calificacionFinal }) {
+    super({ nombre, apellido });
+    this.calificacionFinal = calificacionFinal;
+  }
+
+  set nuevoNombre(nuevoNombre) {
+    this.nombre = nuevoNombre;
+  }
+
+  get calificacion() {
+    return this.calificacionFinal;
+  }
+}
+
+const fernandoClass = new Estudiante({
+  nombre: "Fernando",
+  apellido: "Medina",
+  calificacionFinal: 100,
+});
+
+console.log(fernandoClass.nombre);
+console.log(fernandoClass.calificacion);
+fernandoClass.nuevoNombre = "Danny";
+console.log(fernandoClass.nombre);
+console.log(fernandoClass.calificacion);
+
+class Largometraje {
+  constructor({ nombre }) {
+    this.nombre = nombre;
+  }
+
+  play() {}
+}
+
+class Peli extends Largometraje {
+  constructor({ nuevoNombre }) {
+    super({ nombre: nuevoNombre });
+    this.tipo = "Peli";
+  }
+}
+
+class Docu extends Largometraje {
+  constructor({ nombre }) {
+    super({ nombre });
+    this.tipo = "Docu";
+  }
+}
+
+const pelicula = new Peli({ nuevoNombre: "X" });
+pelicula.play();
