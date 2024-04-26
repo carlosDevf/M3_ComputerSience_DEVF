604e154f4a73678422704880e67868048652c3c8
donCelis <alexcelis019@gmail.com>
2024-04-25 19:43:55 -0500

    iniciar con un crud

---
+++ crud/db.js
@@ -0,0 +1,97 @@
+const jedis = [
+  {
+    name: "Luke Skywalker",
+    age: 30,
+    team: "Rebel Alliance",
+    role: "Maestro Jedi",
+    photo:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Obi-Wan Kenobi",
+    age: 60,
+    team: "Jedi Order",
+    role: "Maestro de la Orden",
+    photo:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Yoda",
+    age: 900,
+    team: "Jedi Order",
+    role: "Maestro de la Orden",
+    photo: null,
+  },
+  {
+    name: "Anakin Skywalker",
+    age: 23,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Mace Windu",
+    age: 50,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  /*  {
+    name: "Ahsoka Tano",
+    age: 18,
+    team: "Jedi Order",
+    role: "Padawan",
+  },
+  {
+    name: "Qui-Gon Jinn",
+    age: 40,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Kit Fisto",
+    age: 30,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Ezra Bridger",
+    age: 20,
+    team: "Rebel Alliance",
+    role: "Padawan",
+  },
+  {
+    name: "Plo Koon",
+    age: 45,
+    team: "Jedi Order",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Rey",
+    age: 25,
+    team: "Resistance",
+    role: "Maestro Jedi",
+  },
+  {
+    name: "Kylo Ren",
+    age: 30,
+    team: "First Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Aayla Secura",
+    age: 35,
+    team: "Jedi Order",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Kanan Jarrus",
+    age: 32,
+    team: "Rebel Alliance",
+    role: "Caballero Jedi",
+  },
+  {
+    name: "Barriss Offee",
+    age: 28,
+    team: "Jedi Order",
+    role: "Padawan",
+  }, */
+];
