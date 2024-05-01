cb6bbdcccac686c9686ab09c466124386162a0d5
donCelis <alexcelis019@gmail.com>
2024-04-30 21:54:33 -0500

    agregar la condicional cuando no hay usuarios

--- crud/src/main.js
+++ crud/src/main.js
@@ -14,15 +14,20 @@ function renderTitlesTable() {
 function renderUsersTable(users = jedis) {
   usersTable.innerHTML = "";
 
+  if (!users.length) {
+    usersTable.innerHTML = "<td colspan='5'><p class='text-center py-4'>No hay usuarios</p></td>";
+    return;
+  }
+
   const fragment = document.createDocumentFragment();
 
-  users.forEach(user => {
+  users.forEach((user) => {
     const tempTr = document.createElement("tr");
     tempTr.className = "hover:bg-gray-50";
     tempTr.innerHTML = trC(user);
 
     fragment.appendChild(tempTr);
-  })
+  });
 
   usersTable.appendChild(fragment);
 }
